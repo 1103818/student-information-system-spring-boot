dc0ab234c62143622876a43bcf90129c06bd8773
FaisaljanBaloch <faisaljanbaloch@gmail.com>
2023-04-03 20:43:55 +0500

    Add validation dependency in the project

--- src/main/java/com/example/studentinfosys/controller/StudentController.java
+++ src/main/java/com/example/studentinfosys/controller/StudentController.java
@@ -3,12 +3,15 @@ package com.example.studentinfosys.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.studentinfosys.model.Student;
 import com.example.studentinfosys.service.StudentService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class StudentController {
@@ -30,8 +33,10 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    String storeStudent(@ModelAttribute Student student) {
-        System.out.println(student);
+    String storeStudent(@Valid @ModelAttribute Student student, Errors errors) {
+        if (errors.hasErrors()) {
+            return "student-form";
+        }
         studentService.addStudent(student);
         return "redirect:/students";
     }
--- src/main/java/com/example/studentinfosys/model/Student.java
+++ src/main/java/com/example/studentinfosys/model/Student.java
@@ -1,8 +1,17 @@
 package com.example.studentinfosys.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class Student {
     private int id;
+
+    @NotBlank(message = "Your name is required")
+    @Size(min = 2, max = 30, message = "Your name must be between 2 and 30 characters")
     private String name;
+    
+    @Min(value = 18)
     private int age;
 
     public int getId() {
